413bb4099d3533f8d69749ed3c1e32ff3baabb93
ayeshankataria <69868181+ayeshankataria@users.noreply.github.com>
2020-12-01 22:17:20 +0000

    Pulled through API icon data

--- src/index.js
+++ src/index.js
@@ -22,25 +22,29 @@ return `${day} ${hours}:${minutes}`
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperature = document.querySelector(".temp");
   let cityName = document.querySelector(".city");
   let currentDate = document.querySelector("#date");
   let descriptionElement = document.querySelector("#description");
+  let weatherIcon = document.querySelector("#icon");
   let feelsLike = document.querySelector("#feels-like");
   let wind = document.querySelector("#wind-speed");
   let humidityElement = document.querySelector("#humidity");
+
   temperature.innerHTML = Math.round(response.data.main.temp);
   cityName.innerHTML = response.data.name;
   currentDate.innerHTML = formatDate(response.data.dt * 1000);
   descriptionElement.innerHTML = response.data.weather[0].description;
+  weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  weatherIcon.setAttribute("alt", response.data.weather[0].description);
   feelsLike.innerHTML = Math.round(response.data.main.feels_like);
   wind.innerHTML = Math.round(3.6*response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "86f093aa43690ee890e5cd351bb4c53c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let city = "London";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
